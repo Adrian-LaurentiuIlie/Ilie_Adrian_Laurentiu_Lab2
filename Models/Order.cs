8f6ffe31e2ccfcfbe0c4f45ad60e0d491948902c
ilie.adrian1@outlook.com <ilie.adrian1@outlook.com>
2023-10-23 19:45:09 +0300

    Lab3

--- Models/Order.cs
+++ Models/Order.cs
@@ -9,6 +9,7 @@ namespace Ilie_Adrian_Laurentiu_Lab2.Models
         public int OrderID { get; set; }
         public int CustomerID { get; set; }
         public int BookID { get; set; }
+        public DateTime OrderDate { get; set; }
 
         public Customer Customer { get; set; }
         public Book Book { get; set; }
