57ef0694e738d801abfb5b9ea57cf4c70f7eae20
ilie.adrian1@outlook.com <ilie.adrian1@outlook.com>
2023-10-28 13:23:17 +0300

    Lab7

---
+++ LibraryModel/Models/City.cs
@@ -0,0 +1,16 @@
+﻿using Ilie_Adrian_Laurentiu_Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryModel.Models
+{
+    public class City
+    {
+        public int ID { get; set; }
+        public string CityName { get; set; }
+        public ICollection<Customer>? Customers { get; set; }
+    }
+}
